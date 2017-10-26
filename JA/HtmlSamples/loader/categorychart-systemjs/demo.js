edc3dbfe179fadcc234f6dbf0471d06c070b93b0
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-10-26 12:23:35 -0400

    SB build 17.2.223

---
+++ JA/HtmlSamples/loader/categorychart-systemjs/demo.js
@@ -0,0 +1,3 @@
+$(function () {
+System.import("http://jp.staging.igniteui.local/17-2/HtmlSamples/loader/categorychart-systemjs.js");
+});
---
+++ JA/HtmlSamples/notifier/basic-usage/demo.js
@@ -0,0 +1,36 @@
+$(function () {
+           $("#successEditor1").igNotifier({
+                direction: "right",
+                messages: {
+                    info: "情報メッセージ。",
+                    success: "成功メッセージ。",
+                    warning: "警告メッセージ。",
+                    error: "エラー メッセージ。"
+                }
+            }).igNotifier("notify", "success");
+
+            $("#state").on("change", function myfunction() {
+                var state = $(this).find("option:selected").val();
+
+                $("#successEditor1").igNotifier("option", "state", state);
+            });
+
+            $("#mode").on("change", function myfunction() {
+                var mode = $(this).find("option:selected").val();
+
+                $("#successEditor1").igNotifier("option", "mode", mode);
+            });
+
+            $("#direction").on("change", function myfunction() {
+                var direction = $(this).find("option:selected").val();
+
+                $("#successEditor1").igNotifier("option", "direction", direction);
+            });
+
+            $("#show").on("click", function myfunction() {
+                $("#successEditor1").igNotifier("show");
+            });
+            $("#hide").on("click", function myfunction() {
+                $("#successEditor1").igNotifier("hide");
+            });
+        });
