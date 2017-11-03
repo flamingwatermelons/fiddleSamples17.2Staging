f1974d0959393ea554c7e5ec34bded6ac31bdba6
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-11-03 13:45:01 -0400

    SB build 17.2.239

--- EN/HtmlSamples/data-chart/scatter-series/demo.js
+++ EN/HtmlSamples/data-chart/scatter-series/demo.js
@@ -1,11 +1,11 @@
 $(function () {
-            function createScatterChart(selector, seriesType, dataSource) {
+            function createScatterChart(selector, dataSource) {
                 $(selector).igDataChart({
                     width: "320px",
                     height: "320px",
                     dataSource: dataSource,
-                    title: "U.S. Agricultural Production Per Year",
-                    subtitle: "Data from 1961-2007",
+                    title: "Scatter",
+                    subtitle: "U.S. Agricultural Production Per Year",
                     axes: [{
                         name: "xAxis",
                         type: "numericX",
@@ -24,12 +24,94 @@ $(function () {
                     }],
                     series: [{
                         name: "scatter",
-                        type: seriesType,
+                        type: "scatter",
                         xAxis: "xAxis",
                         yAxis: "yAxis",
                         xMemberPath: "Year",
                         yMemberPath: "Value",
-                        markerType: "circle"
+                        markerType: "circle",
+                        title: "Scatter",
+                        showTooltip: true
+                    }],
+                    horizontalZoomable: true,
+                    verticalZoomable: true,
+                    windowResponse: "immediate"
+                });
+            }
+
+            function createScatterLineChart(selector, dataSource) {
+                $(selector).igDataChart({
+                    width: "320px",
+                    height: "320px",
+                    dataSource: dataSource,
+                    title: "Scatter Line",
+                    subtitle: "U.S. Agricultural Production Per Year",
+                    axes: [{
+                        name: "xAxis",
+                        type: "numericX",
+                        interval: 10,
+                        title: "Year",
+                    }, {
+                        name: "yAxis",
+                        type: "numericY",
+                        title: "Billions of USD",
+                        maximumValue: 200000,
+                        formatLabel: function (val) {
+                            var bVal = (val / 1000),
+                            rounded = Math.round(bVal * 100) / 100;
+                            return "$" + rounded;
+                        }
+                    }],
+                    series: [{
+                        name: "scatter",
+                        type: "scatterLine",
+                        xAxis: "xAxis",
+                        yAxis: "yAxis",
+                        xMemberPath: "Year",
+                        yMemberPath: "Value",
+                        markerType: "circle",
+                        title: "Scatter Line",
+                        showTooltip: true
+                    }],
+                    horizontalZoomable: true,
+                    verticalZoomable: true,
+                    windowResponse: "immediate"
+                });
+            }
+
+            function createScatterSplineChart(selector, dataSource) {
+                $(selector).igDataChart({
+                    width: "320px",
+                    height: "320px",
+                    dataSource: dataSource,
+                    title: "Scatter Spline",
+                    subtitle: "U.S. Agricultural Production Per Year",
+                    axes: [{
+                        name: "xAxis",
+                        type: "numericX",
+                        interval: 10,
+                        title: "Year",
+                    }, {
+                        name: "yAxis",
+                        type: "numericY",
+                        title: "Billions of USD",
+                        maximumValue: 200000,
+                        formatLabel: function (val) {
+                            var bVal = (val / 1000),
+                            rounded = Math.round(bVal * 100) / 100;
+                            return "$" + rounded;
+                        }
+                    }],
+                    series: [{
+                        name: "scatterSpline",
+                        type: "scatterSpline",
+                        xAxis: "xAxis",
+                        yAxis: "yAxis",
+                        xMemberPath: "Year",
+                        yMemberPath: "Value",
+                        markerType: "circle",
+                        title: "Scatter Spline",
+                        showTooltip: true
                     }],
                     horizontalZoomable: true,
                     verticalZoomable: true,
@@ -42,8 +124,8 @@ $(function () {
                     width: "320px",
                     height: "320px",
                     dataSource: dataSource,
-                    title: "U.S. Agricultural Production Per Year",
-                    subtitle: "Data from 1961-2007",
+                    title: "Bubble Chart",
+                    subtitle: "U.S. Agricultural Production Per Year",
                     axes: [{
                         name: "xAxis",
                         type: "numericX",
@@ -71,6 +153,8 @@ $(function () {
                         fillMemberPath: "Population",
                         labelMemberPath: "Population",
                         markerType: "circle",
+                        title: "Bubble Chart",
+                        showTooltip: true,
                         radiusScale: {
                             minimumValue: 2,
                             maximumValue: 12,
@@ -94,7 +178,7 @@ $(function () {
                     width: "320px",
                     height: "320px",
                     dataSource: dataSource,
-                    title: "$$(Chart_title_scatter)",
+                    title: "$$(Chart_sel_scatterArea)",
                     subtitle: "$$(Chart_subtitle_scatter)",
                     axes: [{
                         name: "xAxis",
@@ -113,6 +197,8 @@ $(function () {
                         xMemberPath: "X",
                         yMemberPath: "Y",
                         colorMemberPath: "Z",
+                        title: "$$(Chart_sel_scatterArea)",
+                        showTooltip: true,
                         colorScale: {
                             palette: ["red", "orange", "yellow"],
                             interpolationMode: "interpolateRGB",
@@ -128,7 +214,7 @@ $(function () {
                     width: "320px",
                     height: "320px",
                     dataSource: dataSource,
-                    title: "$$(Chart_title_scatter)",
+                    title: "$$(Chart_sel_scatterContour)",
                     subtitle: "$$(Chart_subtitle_scatter)",
                     axes: [{
                         name: "xAxis",
@@ -147,6 +233,8 @@ $(function () {
                         xMemberPath: "X",
                         yMemberPath: "Y",
                         valueMemberPath: "Z",
+                        title: "$$(Chart_sel_scatterContour)",
+                        showTooltip: true,
                         fillScale: {
                             type: "value",
                             brushes: ["red", "orange", "yellow"],
@@ -159,10 +247,10 @@ $(function () {
             }
 
             var dataSource = agriculturalData;
-            createScatterChart("#chartScatter", "scatter", dataSource);
-            createScatterChart("#chartScatterLine", "scatterLine", dataSource);
+            createScatterChart("#chartScatter", dataSource);
+            createScatterLineChart("#chartScatterLine", dataSource);
             createBubbleChart("#chartBubble", dataSource);
-            createScatterChart("#chartScatterSpline", "scatterSpline", dataSource);
+            createScatterSplineChart("#chartScatterSpline", dataSource);
             createScatterAreaChart("#chartScatterArea", scatterData);
             createScatterContourChart("#chartScatterContour", scatterData);
         });
