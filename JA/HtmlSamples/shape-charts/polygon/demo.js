838d8498a43a5e9cc52aa4f8f7bd0b68e6a276c4
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-11-07 04:28:51 -0500

    SB build 17.2.240

--- JA/HtmlSamples/shape-charts/polygon/demo.js
+++ JA/HtmlSamples/shape-charts/polygon/demo.js
@@ -1,36 +1,16 @@
 $(function () {
 (function () {
-            var roomData = [
-                { "Label": "", "Points": [[{ x: 0, y: 0 }, { x: 10, y: 0 }, { x: 10, y: 7 }, { x: 7, y: 7 }, { x: 7, y: 10 }, { x: 0, y: 10 }], ] },
-                { "Label": "Master Bedroom", "Points": [[{ x: 0, y: 0 }, { x: 4, y: 0 }, { x: 4, y: 5 }, { x: 0, y: 5 }], ] },
-                { "Label": "Guest Bedroom", "Points": [[{ x: 2, y: 10 }, { x: 7, y: 10 }, { x: 7, y: 7 }, { x: 2, y: 7 }], ] },
-                { "Label": "Bath", "Points": [[{ x: 0, y: 10 }, { x: 2, y: 10 }, { x: 2, y: 7 }, { x: 0, y: 7 }], ] },
-                { "Label": "Kitchen", "Points": [[{ x: 6, y: 7 }, { x: 10, y: 7 }, { x: 10, y: 4 }, { x: 6, y: 4 }], ] },
-                { "Label": "Living Room", "Points": [[{ x: 6, y: 4 }, { x: 10, y: 4 }, { x: 10, y: 0 }, { x: 6, y: 0 }], ] },
-            ];
-
-            $("#polygonChart").igDataChart({
+            $("#polygonChart").igShapeChart({
                 width: "98%",
                 height: "550px",
                 title: "家の間取り図",
-                dataSource: roomData,
+                dataSource: createRoomData(),
+                chartType: "polygon",
+                outlines: ["white"],
                 isHorizontalZoomEnabled: true,
                 isVerticalZoomEnabled: true,
-                axes: [{
-                    name: "xAxis",
-                    type: "numericX",
-                }, {
-                    name: "yAxis",
-                    type: "numericY",
-                }],
-                series: [{
-                    name: "polygonSeries",
-                    type: "scatterPolygon",
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    shapeMemberPath: "Points",
-                    outline: "white",
-                    markerTemplate: {
+                seriesAdded: function (evt, args) {
+                    args.series.markerTemplate = {
                         render: function (renderInfo) {
                             renderInfo.context.font = "16px serif";
                             renderInfo.context.fillStyle = "white";
@@ -38,7 +18,17 @@ $(function () {
                             renderInfo.context.fillText(renderInfo.data.item().Label, renderInfo.xPosition, renderInfo.yPosition);
                         }
                     }
-                }],
+                }
             });
+
+            function createRoomData() {
+                return [
+                    { "Label": "", "Points": [[{ x: 0, y: 0 }, { x: 10, y: 0 }, { x: 10, y: 7 }, { x: 7, y: 7 }, { x: 7, y: 10 }, { x: 0, y: 10 }], ] },
+                    { "Label": "Master Bedroom", "Points": [[{ x: 0, y: 0 }, { x: 4, y: 0 }, { x: 4, y: 5 }, { x: 0, y: 5 }], ] },
+                    { "Label": "Guest Bedroom", "Points": [[{ x: 2, y: 10 }, { x: 7, y: 10 }, { x: 7, y: 7 }, { x: 2, y: 7 }], ] },
+                    { "Label": "Bath", "Points": [[{ x: 0, y: 10 }, { x: 2, y: 10 }, { x: 2, y: 7 }, { x: 0, y: 7 }], ] },
+                    { "Label": "Kitchen", "Points": [[{ x: 6, y: 7 }, { x: 10, y: 7 }, { x: 10, y: 4 }, { x: 6, y: 4 }], ] },
+                    { "Label": "Living Room", "Points": [[{ x: 6, y: 4 }, { x: 10, y: 4 }, { x: 10, y: 0 }, { x: 6, y: 0 }], ] }];
+            };
         })();
 });
